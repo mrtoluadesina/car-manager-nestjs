153bf9f7f347146eff40431e8a4fd106d960d66b
Tolu Adesina <tolu.adesina@benestudio.co>
2021-05-12 14:11:15 +0100

    api using mock data file

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { CarModule } from './car/car.module';
+import {MongooseModule} from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/car/car_manager'),
+    CarModule],
+})
+export class AppModule {}
